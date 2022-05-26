b66e24a7566e78f2145156f1cc03f98250b7d271
Ibrahim  Habib <58874664+Millz147@users.noreply.github.com>
2022-05-26 09:20:05 +0100

    Update register.php

--- register.php
+++ register.php
@@ -105,11 +105,6 @@ $sql1 = "INSERT INTO employee (utitle,sname,fname,oname,gender,dob,marital,natio
 
 
 <!DOCTYPE HTML>
-<!--
-	ZeroFour by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
 		<title>Pension</title>
